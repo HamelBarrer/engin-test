e3677e2aa1db12507ef927ff2181fe7dc5111621
Hamel Yesid Barrero Castaño <hamel_-barrero@hotmail.com>
2024-02-08 19:02:51 -0500

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+import ModalOrganism from './components/ui/organisms/ModalOrganism';
+
 const App = () => {
-  return <div>App</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="h-screen grid place-content-center">
+      <button
+        className="bg-indigo-500 p-2 rounded-lg text-white font-bold"
+        onClick={() => setIsOpen(true)}
+      >
+        Add
+      </button>
+      {isOpen && <ModalOrganism setIsOpen={setIsOpen} />}
+    </div>
+  );
 };
 
 export default App;
